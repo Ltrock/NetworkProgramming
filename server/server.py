e6f46a70aa813ee6f77bbfe040b1bb8ae45ec738
Ltrock <lookket.111@gmail.com>
2019-12-09 09:16:21 +0700

    add comment

--- server/server.py
+++ server/server.py
@@ -4,19 +4,22 @@ import os
 
 class ServerThreading():
     "run in terminal: python3 server.py"
+    "communicate with more than one client at the same time in the same network"
     def __init__(self):
         self.host = socket.gethostname()
         self.port = 12345
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #socket.setsockopt(level, optname, value)
+#Set the value of the given socket option,SO_REUSEADDR flag tells the kernel to reuse a local socket in TIME_WAIT state, 
+#without waiting for its natural timeout to expire.
         self.s.bind((self.host, self.port))                         
 
     def listen(self):
-        self.s.listen(5)
+        self.s.listen(5) #queue of 5
         while True:
             c, addr = self.s.accept()
             c.settimeout(180)
-            threading.Thread(target = self.listenToClient,args = (c,addr)).start()
+            threading.Thread(target = self.listenToClient,args = (c,addr)).start() #create thread when there is a client connection 
 
     def listenToClient(self, c, addr):
         print("Connect with client", addr)
@@ -44,7 +47,7 @@ class ServerThreading():
                 UploadFile.close()
                 c.close()
         elif (data.decode() == "upload"):
-            FileName = c.recv(1024)
+            FileName = c.recv(1024) # a buffer size of 1024 bytes at a time
             downfile = FileName.decode()
             Data = c.recv(1024)
             DownloadFile = open(downfile,"wb")
